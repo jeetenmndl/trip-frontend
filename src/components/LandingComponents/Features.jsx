ec79bc824c0b7832f655e4fcca714ac13668ef8b
Jeeten Mandal <jeeten.cloveit@gmail.com>
2026-04-06 15:44:42 +0545

    features map done

--- src/components/LandingComponents/Features.jsx
+++ src/components/LandingComponents/Features.jsx
@@ -1,22 +1,49 @@
 import React from 'react'
 import FeaturesCard from '../common/FeaturesCard'
+import { PhoneCall, ShieldCheck, Smile, Zap } from 'lucide-react'
 
 const Features = () => {
+
+  let featuresData = [
+    {
+      title: "24/7 Support",
+      description: "lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaque  adipisicing elit. Voluptas, eaque.",
+      Icon: PhoneCall
+    },
+    {
+      title: "Secure Payments",
+      description: "We ensure safe transactions with advanced encryption and reliable systems protecting your financial data.",
+      Icon: ShieldCheck
+    },
+    {
+      title: "Fast Performance",
+      description: "Experience lightning fast loading speeds and smooth interactions optimized for efficiency and productivity.",
+      Icon: Zap
+    },
+    {
+      title: "User Friendly",
+      description: "Our intuitive interface makes navigation simple, helping users easily access features without confusion or delay.",
+      Icon: Smile
+    }
+  ]
   return (
     <section className='px-20 py-20'>
 
-        {/* heading */}
-        <div>
-            <h2 className='text-5xl font-bold text-center mb-16'>Our Features</h2>
-        </div>
+      {/* heading */}
+      <div>
+        <h2 className='text-5xl font-bold text-center mb-16'>Our Features</h2>
+      </div>
 
-        {/* content  */}
-        <div className='grid grid-cols-4 gap-6'>
-            <FeaturesCard />
-            <FeaturesCard />
-            <FeaturesCard />
-            <FeaturesCard />
-        </div>
+      {/* content  */}
+      <div className='grid grid-cols-4 gap-6'>
+        {
+          featuresData.map((feature, index)=>{
+            return (
+              <FeaturesCard feature={feature} key={index} />
+            )
+          })
+        }
+      </div>
 
     </section>
   )
--- src/components/common/FeaturesCard.jsx
+++ src/components/common/FeaturesCard.jsx
@@ -1,14 +1,14 @@
 import { Angry, User } from 'lucide-react'
 import React from 'react'
 
-const FeaturesCard = () => {
+const FeaturesCard = ({feature}) => {
   return (
     <div className='border border-gray-300 rounded-lg p-4'>
-        <Angry size={40} className='mb-4 text-blue-600' />
+        <feature.Icon size={40} className='mb-4 text-blue-600' />
 
-        <h3 className='text-2xl font-medium mb-4'>24/7 Availability</h3>
+        <h3 className='text-2xl font-medium mb-4'>{feature.title}</h3>
 
-        <p className='text-gray-600'>Our team is available around the clock to assist you with any questions or concerns.</p>
+        <p className='text-gray-600'>{feature.description}</p>
     </div>
   )
 }
