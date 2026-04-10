f193ddc4ab6bd2a83614682be903d6feaf395edf
Jeeten Mandal <jeeten.cloveit@gmail.com>
2026-04-10 16:26:44 +0545

    form done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import About from './pages/About'
 import Landing from './pages/Landing'
+import Register from './pages/Register'
 
 const App = () => {
 
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/about" element={<About />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
    </BrowserRouter>
   )
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
+import { Controller, useForm } from "react-hook-form"
 import * as z from "zod"
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Field, FieldError, FieldLabel } from '@/components/ui/field'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
 const formSchema = z.object({
     name: z.string().min(3, "Name must be at least 3 characters"),
@@ -9,7 +21,7 @@ const formSchema = z.object({
     password: z.string().min(8, "Password must be at least 8 characters"),
     confirmPassword: z.string().min(8, "Password must be at least 8 characters")
 }).refine((data) => {
-    data.password === data.confirmPassword
+    return data.password === data.confirmPassword
 },
     {
         message: "Passwords don't match",
@@ -29,13 +41,74 @@ const Register = () => {
         }
     })
 
-    const onSubmit = (data) =>{
+    const onSubmit = (data) => {
         console.log(data)
     }
 
     return (
         <form onSubmit={form.handleSubmit(onSubmit)}>
-            
+            <Card className="w-1/4 mx-auto mt-24">
+                <CardHeader className={"border-b"}>
+                    <CardTitle>Register to CloveTrip</CardTitle>
+                    <CardDescription>Enter your details to get started</CardDescription>
+                </CardHeader>
+
+                <CardContent className="space-y-3">
+                    <Controller
+                        name="name"
+                        control={form.control}
+                        render={({ field, fieldState }) => (
+                            <Field data-invalid={fieldState.invalid}>
+                                <FieldLabel htmlFor={field.name}>Name</FieldLabel>
+                                <Input {...field} placeholder="John Doe" id={field.name} aria-invalid={fieldState.invalid} />
+                                {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                            </Field>
+                        )}
+                    />
+
+                     <Controller
+                        name="email"
+                        control={form.control}
+                        render={({ field, fieldState }) => (
+                            <Field data-invalid={fieldState.invalid}>
+                                <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                                <Input type="email" placeholder="abc@gmail.com" {...field} id={field.name} aria-invalid={fieldState.invalid} />
+                                {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                            </Field>
+                        )}
+                    />
+
+                     <Controller
+                        name="password"
+                        control={form.control}
+                        render={({ field, fieldState }) => (
+                            <Field data-invalid={fieldState.invalid}>
+                                <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                                <Input type="password" placeholder="••••••••" {...field} id={field.name} aria-invalid={fieldState.invalid} />
+                                {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                            </Field>
+                        )}
+                    />
+
+                    
+                     <Controller
+                        name="confirmPassword"
+                        control={form.control}
+                        render={({ field, fieldState }) => (
+                            <Field data-invalid={fieldState.invalid}>
+                                <FieldLabel htmlFor={field.name}>Confirm Password</FieldLabel>
+                                <Input type="password" placeholder="••••••••" {...field} id={field.name} aria-invalid={fieldState.invalid} />
+                                {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                            </Field>
+                        )}
+                    />
+                </CardContent>
+
+                <CardFooter>
+                    <Button size="lg" className="w-full" type="submit">Submit</Button>
+                </CardFooter>
+
+            </Card>
         </form>
     )
 }
