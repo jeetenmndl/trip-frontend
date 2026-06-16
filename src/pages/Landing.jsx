47d1e515a752d33813c89df24a4c3692ac85175d
Jeeten Mandal <jeeten.cloveit@gmail.com>
2026-06-16 15:55:48 +0545

    add About, Gallery, Connections, Testimonials, and Footer components to Landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,15 +4,25 @@ import Hero from '../components/LandingComponents/Hero'
 import Features from '../components/LandingComponents/Features'
 import FAQ from '@/components/LandingComponents/FAQ'
 import Contact from '@/components/LandingComponents/Contact'
+import { About } from '@/components/LandingComponents/About'
+import { Gallery } from '@/components/LandingComponents/Gallery'
+import { Footer } from '@/components/LandingComponents/Footer'
+import { Testimonials } from '@/components/LandingComponents/Testimonials'
+import { Connections } from '@/components/LandingComponents/Connections'
 
 const Landing = () => {
   return (
     <div> 
         <Navbar />
         <Hero />
+        <About />
         <Features />
+        <Gallery />
+        <Connections />
+        <Testimonials />
         <Contact />
         <FAQ />
+        <Footer />
     </div>
   )
 }
